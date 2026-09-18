279dc973555f0427284afca8a4f4f5c5ea99e654
Suraj Thiyagaraj <suraj@tangerinedesigns.studio>
2026-09-18 16:48:58 +0530

    Template update

--- apps/views.py
+++ apps/views.py
@@ -4,15 +4,17 @@ from django.http import HttpResponse
 
 posts=[
     {
-        'Name':'SijuYogi'
+        'Name':'SijuYogi',
+        'Age':27
     },
     {
-        'Name' :'VinithXavier'
+        'Name' :'VinithXavier',
+        'Age':28
     }
 ]
 
 def home(request):
-    return render(request,'apps/home.html')
+    return render(request,'apps/home.html', {'title':'App-Home'})
 def about(request):
     return render(request,'apps/about.html')
 def contact(request):
